391fda3881e961deb2f0cbdd64a3a8d13a421a85
Marvin Kome <marvinkome@gmail.com>
2022-08-13 17:06:32 +0200

    add main page scaffold

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import NextHead from "next/head";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const theme = extendTheme({
+  fonts: {
+    body: "'Nunito', sans-serif",
+    heading: "'Nunito', sans-serif",
+  },
+  styles: {
+    global: {
+      body: {
+        bgColor: "gray.50",
+      },
+    },
+  },
+});
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <NextHead>
+        <title>Hal Test</title>
+      </NextHead>
+
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default App;
